686824b9511bd344bb842c0c6d205a5156d997f0
PapiczeK <riptidesurawp@gmail.com>
2017-09-01 20:46:24 +0200

    init przedszkolanka

---
+++ easy/przedszk.py
@@ -0,0 +1,24 @@
+counterTires = int(input())
+answers = []
+
+def CandyCounter(numChildren):
+	grupChildrenOne = numChildren[0]
+	grupChildrenTwo = numChildren[1]
+	if numChildren[0] == numChildren[1]:
+		return numChildren[0]
+	else:
+		while numChildren[0] == numChildren[1]:
+			if numChildren[0] > numChildren[1]:
+				numChildren[1] += grupChildrenTwo
+			elif numChildren[0] < numChildren[1]:
+				numChildren[0] += grupChildrenOne
+	return numChildren[0] 
+
+for x in range(counterTires):
+	numberOfChildren = input()
+	numberOfChildren = numberOfChildren.split(' ')
+	numberOfChildren = map(int,numberOfChildren)
+	answers.append(CandyCounter(numberOfChildren))
+
+for x in answers:
+	print(x)
