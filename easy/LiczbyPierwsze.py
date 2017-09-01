8f0261e2c48833542c5a084d432e30376f0ce066
PapiczeK <riptidesurawp@gmail.com>
2017-09-01 19:41:45 +0200

    End LiczbyPierwsze

--- easy/LiczbyPierwsze.py
+++ easy/LiczbyPierwsze.py
@@ -1,18 +1,32 @@
 #Sprawdź, które spośród danych liczb są liczbami pierwszymi
 
+import math
+
 counterTests = input()
 answers = []
 
+def CheckPrimeNumber(number):
+	divider = 2
+	sqrtNumber = math.sqrt(number)
+	while sqrtNumber >= divider:
+		if number % divider  == 0:
+#			print(number%divider)
+			return False
+		else:
+			divider+=1
+#			print(number%divider)
+	return True
+
 for test in range(int(counterTests)):
-	number = input()
-	checkingNumber = sqrt(int(number))
-        divider = 2
-        while checkingNumber >= divider:
-                if checkingNumber % divider == 0 or checkingNumber == 1:
-                        return 'NIE'
-                divider += 1
-        
-	answers.append(CheckPrimeNumber(number))
+	number = int(input())
+	number = int(number)
+	if number > 1:
+		if CheckPrimeNumber(number):
+			answers.append('TAK')
+		else:
+	        	answers.append('NIE')
+	else:
+		answers.append('NIE')
 
 for x in answers:
 	print(x)
